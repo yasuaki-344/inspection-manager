3607edb18021efefcae4995ba92ee746760d1265
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-03-02 23:24:53 +0900

    Refactor Counter component

---
+++ src/Web/ClientApp/src/components/Counter.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export const Counter = (): JSX.Element => {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counter</h1>
+
+      <p>This is a simple example of a React component.</p>
+
+      <p aria-live="polite">Current count: <strong>{count}</strong></p>
+
+      <button className="btn btn-primary" onClick={incrementCounter}>Increment</button>
+    </div>
+  );
+}
+Counter.displayName = Counter.name;
