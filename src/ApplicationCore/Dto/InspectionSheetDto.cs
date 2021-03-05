0c01a35e798428f9100a1a0b889d9e287236ec10
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-03-05 22:31:58 +0900

    Create InspectionSheetDto.cs

---
+++ src/ApplicationCore/Dto/InspectionSheetDto.cs
@@ -0,0 +1,16 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+using System.Text.Json.Serialization;
+
+namespace InspectionManager.ApplicationCore.Dto
+{
+    public class InspectionSheetDto
+    {
+        [JsonPropertyName("sheet_name")]
+        public string SheetName { get; set; } = string.Empty;
+    }
+}
