bc7b2a3e5308f27ebf0ebb21a801f0a9cc107181
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-03-05 22:37:06 +0900

    Create InspectionSheetRepository.cs

---
+++ src/Infrastructure/InspectionSheetRepository.cs
@@ -0,0 +1,26 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+
+using System;
+using InspectionManager.ApplicationCore.Dto;
+using InspectionManager.ApplicationCore.Interfaces;
+
+namespace InspectionManager.Infrastructure
+{
+    public class InspectionSheetRepository : IInspectionSheetRepository
+    {
+        public InspectionSheetRepository()
+        {
+
+        }
+
+        public void CreateInspectionSheet(InspectionSheetDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
