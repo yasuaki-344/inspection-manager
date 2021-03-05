07252d52b279f9fc1b9b6cb600cff4c9bd8f2370
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-03-05 23:10:54 +0900

    Update Create.tsx

--- src/Web/ClientApp/src/inspection/Create.tsx
+++ src/Web/ClientApp/src/inspection/Create.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 export const Create = (): JSX.Element => {
+  const handleButtonClick = () => {
+    console.log("button clicked")
+  }
+
   return (
     <div>
       <h1>新規作成ページ</h1>
+      <button onClick={handleButtonClick}>新規作成</button>
     </div>
   );
 }
