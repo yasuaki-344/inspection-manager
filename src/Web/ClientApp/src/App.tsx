c7e28d72228f28455ef94ff7e814297a2954b656
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-07 13:43:16 +0900

    Update App.tsx

--- src/Web/ClientApp/src/App.tsx
+++ src/Web/ClientApp/src/App.tsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Create } from './inspection/Create';
+import { Details } from './inspection/Details';
 
 import './custom.css'
 
@@ -11,6 +12,7 @@ const App = (): JSX.Element => {
     <Layout>
       <Route exact path='/' component={Home} />
       <Route path='/create' component={Create} />
+      <Route path='/details/:id' component={Details} />
     </Layout>
   );
 }
