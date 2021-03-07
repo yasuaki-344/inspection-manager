1753ac685a9e4236cb7eea08f2a742ec5e4b6b81
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-07 13:56:03 +0900

    Update Details.tsx

--- src/Web/ClientApp/src/inspection/Details.tsx
+++ src/Web/ClientApp/src/inspection/Details.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Details = ({ match }: any): JSX.Element => {
+  const [inspectionSheet, setInspectionSheet] = useState({
+    sheet_id: '',
+    sheet_name: '',
+  });
+
+  useEffect(() => {
+    fetch(`inspectionsheet/${match.params.id}`)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        setInspectionSheet(json);
+      })
+      .catch(console.error);
+  }, []);
 
   return (
     <div>
       <h1>詳細ページ</h1>
-      <h2>id:{match.params.id}</h2>
+      <h3>id:{inspectionSheet.sheet_id}</h3>
+      <h3>name:{inspectionSheet.sheet_name}</h3>
     </div>
   );
 }
