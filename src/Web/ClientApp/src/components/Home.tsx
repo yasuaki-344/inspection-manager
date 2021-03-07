b743746d53c19596257a12ce018e4508c8856af5
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-07 18:40:42 +0900

    Modify table design

--- src/Web/ClientApp/src/components/Home.tsx
+++ src/Web/ClientApp/src/components/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
 import { InspectionSheet } from '../inspection/Types';
 
 export const Home = (): JSX.Element => {
@@ -26,7 +27,7 @@ export const Home = (): JSX.Element => {
         console.log(json);
         setInspectionSheets(
           inspectionSheets.filter((x: InspectionSheet) =>
-          x.sheet_id !== json.sheet_id)
+            x.sheet_id !== json.sheet_id)
         );
       })
       .catch(console.error);
@@ -36,26 +37,37 @@ export const Home = (): JSX.Element => {
     <div>
       <h1>点検シート一覧</h1>
       <Link to="/create">新規作成</Link>
-      <table>
-        <thead>
-          <tr>
-            <th>シート名</th>
-            <th>&nbsp;</th>
-          </tr>
-        </thead>
-        <tbody>
-          {inspectionSheets.map((sheet: any) =>
-            <tr key={sheet.sheet_id}>
-              <td>{sheet.sheet_name}</td>
-              <td>
-                <Link to={"/edit/" + sheet.sheet_id}>編集</Link>|
+      <TableContainer component={Paper}>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>点検シート名</TableCell>
+              <TableCell>&nbsp;</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {inspectionSheets.map((sheet: any) =>
+              <TableRow key={sheet.sheet_id}>
+                <TableCell>
+                  {sheet.sheet_name}
+                </TableCell>
+                <TableCell>
+                  <Link to={"/edit/" + sheet.sheet_id}>編集</Link>|
                 <Link to={"/details/" + sheet.sheet_id}>詳細</Link>|
-                <button onClick={() => handleDelete(sheet.sheet_id)}>削除</button>
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+                <Button
+                  size='small'
+                  variant='contained'
+                  color='secondary'
+                  onClick={() => handleDelete(sheet.sheet_id)}
+                >
+                  削除
+                </Button>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
