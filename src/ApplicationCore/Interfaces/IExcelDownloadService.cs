19879696493a81be87eaaeeb6c4a536ce22a71b6
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-08 22:26:24 +0900

    Create new interface

---
+++ src/ApplicationCore/Interfaces/IExcelDownloadService.cs
@@ -0,0 +1,13 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+
+namespace InspectionManager.ApplicationCore.Interfaces
+{
+    public interface IExcelDownloadService
+    {
+    }
+}
---
+++ src/ApplicationCore/Services/ExcelDownloadService.cs
@@ -0,0 +1,25 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+
+using Microsoft.Extensions.Logging;
+
+namespace InspectionManager.ApplicationCore.Interfaces
+{
+    public class ExcelDownloadService : IExcelDownloadService
+    {
+        private readonly ILogger<ExcelDownloadService> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of ExcelDownloadService class.
+        /// </summary>
+        /// <param name="logger">logger object</param>
+        public ExcelDownloadService(ILogger<ExcelDownloadService> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
