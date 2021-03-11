3feea20cae030ec3888392a8afc697e18948e51a
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-11 23:08:16 +0900

    Add new dto

--- src/ApplicationCore/Dto/EquipmentDto.cs
+++ src/ApplicationCore/Dto/EquipmentDto.cs
@@ -4,6 +4,7 @@
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 //
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace InspectionManager.ApplicationCore.Dto
@@ -15,5 +16,8 @@ namespace InspectionManager.ApplicationCore.Dto
 
         [JsonPropertyName("equipment_name")]
         public string EquipmentName { get; set; } = string.Empty;
+
+        [JsonPropertyName("inspection_items")]
+        public List<InspectionItemDto> Equipments { get; set; } = new List<InspectionItemDto>();
     }
 }
---
+++ src/ApplicationCore/Dto/InspectionItemDto.cs
@@ -0,0 +1,19 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+using System.Text.Json.Serialization;
+
+namespace InspectionManager.ApplicationCore.Dto
+{
+    public class InspectionItemDto
+    {
+        [JsonPropertyName("inspection_item_id")]
+        public string InspectionItemId { get; set; } = string.Empty;
+
+        [JsonPropertyName("inspection_content")]
+        public string InspectionContent { get; set; } = string.Empty;
+    }
+}
