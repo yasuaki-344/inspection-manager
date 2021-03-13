b1201ef498a83567e8368db8ab65ca3d0bc07f17
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-13 12:15:43 +0900

    Modify design of InspectionSheetForm

--- src/Web/ClientApp/src/inspection/InspectionSheetForm.tsx
+++ src/Web/ClientApp/src/inspection/InspectionSheetForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Grid, TextField } from '@material-ui/core';
+import { Button, Grid, Paper, TextField } from '@material-ui/core';
 import { EquipmentForm } from './EquipmentForm';
 import { Equipment } from './Types';
 
@@ -22,40 +22,42 @@ export const InspectionSheetForm = (props: any): JSX.Element => {
     : <></>;
 
   return (
-    <Grid container spacing={2}>
-      {contents}
-      <Grid item xs={12}>
-        <TextField
-          required
-          id="outlined-required"
-          label="点検シート名"
-          variant="outlined"
-          size="small"
-          name="sheet_name"
-          value={props.sheet.sheet_name}
-          onChange={e => {
-            props.updateField(e);
-          }}
-        />
+    <Paper>
+      <Grid container spacing={2}>
+        {contents}
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="outlined-required"
+            label="点検シート名"
+            variant="outlined"
+            size="small"
+            name="sheet_name"
+            value={props.sheet.sheet_name}
+            onChange={e => {
+              props.updateField(e);
+            }}
+          />
+        </Grid>
+        {props.sheet.equipments.map((equipment: Equipment) =>
+          <Grid item xs={12} key={equipment.equipment_id}>
+            <EquipmentForm
+              equipment={equipment}
+              addEquipment={props.addEquipment}
+              removeEquipment={props.removeEquipment}
+              updateEquipment={props.updateEquipment}
+              addInspectionItem={props.addInspectionItem}
+              removeInspectionItem={props.removeInspectionItem}
+              updateInspectionItem={props.updateInspectionItem}
+            />
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          <Button size='medium' variant='contained' color='primary'
+            onClick={props.addEquipment}>機器追加</Button>
+        </Grid>
       </Grid>
-      {props.sheet.equipments.map((equipment: Equipment) =>
-        <EquipmentForm
-          key={equipment.equipment_id}
-          equipment={equipment}
-          addEquipment={props.addEquipment}
-          removeEquipment={props.removeEquipment}
-          updateEquipment={props.updateEquipment}
-          addInspectionItem={props.addInspectionItem}
-          removeInspectionItem={props.removeInspectionItem}
-          updateInspectionItem={props.updateInspectionItem}
-        />
-      )}
-      <Grid item xs={12}>
-        <Button size='medium' variant='contained' color='primary'
-          onClick={props.addEquipment}>機器追加</Button>
-      </Grid>
-
-    </Grid>
+    </Paper>
   );
 }
 InspectionSheetForm.displayName = InspectionSheetForm.name;
