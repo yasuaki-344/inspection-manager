7aa71fdcb1ebee0ab1d256dbad25ae9673f7e203
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-20 13:55:16 +0900

    Create ExcelSheetController.cs

---
+++ src/Web/Controllers/ExcelSheetController.cs
@@ -0,0 +1,38 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using InspectionManager.ApplicationCore.Dto;
+using InspectionManager.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InspectionManager.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExcelSheetController : ControllerBase
+    {
+        private readonly ILogger<ExcelSheetController> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of ExcelSheetController class.
+        /// </summary>
+        /// <param name="logger">logger object</param>
+        public ExcelSheetController(
+            ILogger<ExcelSheetController> logger
+        )
+        {
+            _logger = logger;
+        }
+    }
+}
