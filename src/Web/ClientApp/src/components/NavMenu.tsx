07c20aaa4d91d971593c8c4d29f667bfc8f6c1e0
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-23 21:41:54 +0900

    Change header design

--- src/Web/ClientApp/src/components/NavMenu.tsx
+++ src/Web/ClientApp/src/components/NavMenu.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { AppBar, Menu, MenuItem, IconButton, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar, Divider, IconButton, Toolbar, Typography,
+  List, ListItem, ListItemIcon,
+  SwipeableDrawer
+} from '@material-ui/core';
+import CreateIcon from '@material-ui/icons/Create';
+import HomeIcon from '@material-ui/icons/Home';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
@@ -8,56 +14,59 @@ import './NavMenu.css';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     textDark: {
-      color: "#000000",
-      textDecoration:"none",
+      color: '#000000',
+      textDecoration: 'none',
     },
     textWhite: {
-      color: "#FFFFFF",
-      textDecoration:"none",
+      color: '#FFFFFF',
+      textDecoration: 'none',
     },
   })
 );
 
 export const NavMenu = (): JSX.Element => {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const [open, setOpen] = useState(false);
 
   return (
     <header>
-      <AppBar position="static">
+      <AppBar position='static'>
         <Toolbar>
-          <IconButton edge="start" aria-label="menu" color="inherit" onClick={handleClick}>
+          <IconButton edge='start' aria-label='menu' color='inherit' onClick={() => setOpen(true)}>
             <MenuIcon />
           </IconButton>
-          <Menu
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
+          <SwipeableDrawer
+            anchor='left'
+            open={open}
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
           >
-            <MenuItem onClick={handleClose}>
-              <Link className={classes.textDark} to="/">Home</Link>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Link className={classes.textDark} to="/create">新規作成</Link>
-            </MenuItem>
-          </Menu>
-          <Typography variant="h6" noWrap color="inherit">
-            <Link className={classes.textWhite} to="/" color="primary">
+            <div
+              role='presentation'
+              onClick={() => setOpen(false)}
+              onKeyDown={() => setOpen(false)}
+            >
+              <List>
+                <ListItem button>
+                  <ListItemIcon><HomeIcon /></ListItemIcon>
+                  <Link className={classes.textDark} to='/'>Home</Link>
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon><CreateIcon /></ListItemIcon>
+                  <Link className={classes.textDark} to='/create'>新規作成</Link>
+                </ListItem>
+              </List>
+              <Divider />
+            </div>
+          </SwipeableDrawer>
+          <Typography variant='h6' noWrap color='inherit'>
+            <Link className={classes.textWhite} to='/' color='primary'>
               Inspection Manager
             </Link>
           </Typography>
         </Toolbar>
       </AppBar>
-    </header>
+    </header >
   );
 }
 NavMenu.displayName = NavMenu.name;
