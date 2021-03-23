4f9de0797ae9f8b56fcbe6b801812bb3d273fb1f
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-23 22:46:09 +0900

    Update InspectionGroupCategory.tsx

--- src/Web/ClientApp/src/categories/InspectionGroupCategory.tsx
+++ src/Web/ClientApp/src/categories/InspectionGroupCategory.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import {
-  IconButton, Grid, Paper,
+  IconButton, Grid, Paper, TextField, Button,
   BottomNavigation, BottomNavigationAction,
   Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
 } from '@material-ui/core';
@@ -9,8 +9,68 @@ import AddCircleIcon from '@material-ui/icons/AddCircle';
 import CancelIcon from '@material-ui/icons/Cancel';
 
 export const InspectionGroupCategory = (): JSX.Element => {
-  const [groups, setGroups] = useState<string[]>([
-    'group1', 'group2', 'group3']);
+  const [groups, setGroups] = useState<string[]>([]);
+  const [disabled, setDisabled] = useState(false);
+
+  useEffect(() => {
+    fetch('inspectiongroup')
+      .then(res => res.json())
+      .then((json: string[]) => {
+        setGroups(json);
+      })
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    setDisabled(!groups.length || groups.includes(''));
+  }, [groups]);
+
+  /**
+   * Implement the process to add new group
+   */
+  const handleAddItem = (): void => setGroups(groups.concat(''));
+
+  /**
+   * Implement the process to update group
+   */
+  const handleUpdateItem = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    index: number): void =>
+    setGroups(groups.map((group: string, i: number) =>
+      (i === index) ? event.target.value : group
+    ));
+
+  /**
+   * Implement the process to delete group
+   */
+  const handleDeleteItem = (index: number): void =>
+    setGroups(groups.filter((item: string, i: number) => i !== index));
+
+  /**
+   * Implement the process to submit inspection groups
+   */
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    fetch('inspectiongroup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(groups)
+    })
+      .then((res) => {
+        if (res.ok) {
+          alert('登録に成功しました');
+        } else {
+          alert('登録に失敗しました')
+        }
+        return res.json();
+      })
+      .then((json: string[]) => {
+        setGroups(json);
+      })
+      .catch(console.error);
+  }
 
   return (
     <Grid container spacing={1}>
@@ -21,41 +81,65 @@ export const InspectionGroupCategory = (): JSX.Element => {
         <Link to='/'>トップページへ戻る</Link>
       </Grid>
       <Grid item xs={12}>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>点検グループ</TableCell>
-                <TableCell>&nbsp;</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {groups.map((group: string, index: number) =>
-                <TableRow key={`group_{index}`}>
-                  <TableCell>{group}</TableCell>
-                  <TableCell padding='checkbox'>
-                    <IconButton
-                      size="small"
-                      color='secondary'
-                    >
-                      <CancelIcon />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>点検グループ</TableCell>
+                      <TableCell>&nbsp;</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {groups.map((group: string, index: number) =>
+                      <TableRow key={`group_{index}`}>
+                        <TableCell>
+                          <TextField
+                            required
+                            variant="outlined"
+                            size="small"
+                            value={group}
+                            onChange={(e) => handleUpdateItem(e, index)}
+                          />
+                        </TableCell>
+                        <TableCell padding='checkbox'>
+                          <IconButton
+                            size="small"
+                            color='secondary'
+                            onClick={() => handleDeleteItem(index)}
+                          >
+                            <CancelIcon />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+            <Grid item xs={12}>
+              <BottomNavigation showLabels>
+                <BottomNavigationAction
+                  label="点検グループ追加"
+                  icon={<AddCircleIcon />}
+                  onClick={handleAddItem}
+                />
+              </BottomNavigation>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type='submit'
+                variant='contained'
+                color='primary'
+                disabled={disabled}
+              >グループ登録</Button>
+            </Grid>
+          </Grid>
+        </form>
       </Grid>
-      <Grid item xs={12}>
-        <BottomNavigation showLabels>
-          <BottomNavigationAction
-            label="点検グループ追加"
-            icon={<AddCircleIcon />}
-          />
-        </BottomNavigation>
-      </Grid>
-    </Grid>
+    </Grid >
   );
 }
 InspectionGroupCategory.displayName = InspectionGroupCategory.name;
