d45a712a6b47c09934bfdca8a32a7419ecdd2110
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-26 00:17:55 +0900

    Add dialog

--- src/Web/ClientApp/src/categories/ChoicesTemplate.tsx
+++ src/Web/ClientApp/src/categories/ChoicesTemplate.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BottomNavigation, BottomNavigationAction, IconButton,
+  Dialog, DialogActions, DialogContent, DialogTitle,
   Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
   Grid, Paper,
 
@@ -10,6 +11,7 @@ import CancelIcon from '@material-ui/icons/Cancel';
 import EditIcon from '@material-ui/icons/Edit';
 
 export const ChoicesTemplate = (): JSX.Element => {
+  const [open, setOpen] = useState(false);
   const templates = [
     ['hoge', 'foo', 'var']
   ];
@@ -18,54 +20,63 @@ export const ChoicesTemplate = (): JSX.Element => {
    * Add new template set.
    */
   const handleAddTemplate = () => {
-
+    setOpen(true);
   };
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <h1>選択肢テンプレート</h1>
-      </Grid>
-      <Grid item xs={12}>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell />
-                <TableCell>選択肢</TableCell>
-                <TableCell />
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {templates.map((choices: string[], index: number) =>
-                <TableRow key={`template_${index}`}>
-                  <TableCell>
-                    <IconButton size='small'>
-                      <EditIcon />
-                    </IconButton>
-                  </TableCell>
-                  <TableCell>{choices.join(',')}</TableCell>
-                  <TableCell align='right'>
-                    <IconButton size='small'>
-                      <CancelIcon />
-                    </IconButton>
-                  </TableCell>
+    <>
+      <Grid container>
+        <Grid item xs={12}>
+          <h1>選択肢テンプレート</h1>
+        </Grid>
+        <Grid item xs={12}>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell />
+                  <TableCell>選択肢</TableCell>
+                  <TableCell />
                 </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
-      <Grid item xs={12}>
-        <BottomNavigation showLabels>
-          <BottomNavigationAction
-            label="テンプレート追加"
-            icon={<AddCircleIcon />}
-            onClick={handleAddTemplate}
-          />
-        </BottomNavigation>
-      </Grid>
-    </Grid >
+              </TableHead>
+              <TableBody>
+                {templates.map((choices: string[], index: number) =>
+                  <TableRow key={`template_${index}`}>
+                    <TableCell>
+                      <IconButton size='small'>
+                        <EditIcon />
+                      </IconButton>
+                    </TableCell>
+                    <TableCell>{choices.join(',')}</TableCell>
+                    <TableCell align='right'>
+                      <IconButton size='small'>
+                        <CancelIcon />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={12}>
+          <BottomNavigation showLabels>
+            <BottomNavigationAction
+              label="テンプレート追加"
+              icon={<AddCircleIcon />}
+              onClick={handleAddTemplate}
+            />
+          </BottomNavigation>
+        </Grid>
+      </Grid >
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>選択肢テンプレート編集</DialogTitle>
+        <DialogContent>
+        </DialogContent>
+        <DialogActions>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 ChoicesTemplate.displayName = ChoicesTemplate.name;
