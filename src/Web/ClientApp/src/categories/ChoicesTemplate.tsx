2bb39a3369aa116d20d1b2f4e0c13f3abec4d645
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-25 23:54:35 +0900

    Add button to add template

--- src/Web/ClientApp/src/categories/ChoicesTemplate.tsx
+++ src/Web/ClientApp/src/categories/ChoicesTemplate.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import {
+  BottomNavigation, BottomNavigationAction,
+  Grid
+} from '@material-ui/core';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 export const ChoicesTemplate = (): JSX.Element => {
+  /**
+   * Add new template set.
+   */
+  const handleAddTemplate = () => {
+
+  };
+
   return (
-    <div>ChoicesTemplate</div>
+    <Grid container>
+      <Grid item xs={12}>
+        <h1>選択肢テンプレート</h1>
+      </Grid>
+      <Grid item xs={12}>
+        <BottomNavigation showLabels>
+          <BottomNavigationAction
+            label="テンプレート追加"
+            icon={<AddCircleIcon />}
+            onClick={handleAddTemplate}
+          />
+        </BottomNavigation>
+      </Grid>
+
+    </Grid>
   );
 }
 ChoicesTemplate.displayName = ChoicesTemplate.name;
