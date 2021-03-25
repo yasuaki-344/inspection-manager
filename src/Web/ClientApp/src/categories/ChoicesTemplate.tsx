25fcc79e3531bfc83586db8e893374619e4f7af9
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-26 00:12:51 +0900

    Add template design

--- src/Web/ClientApp/src/categories/ChoicesTemplate.tsx
+++ src/Web/ClientApp/src/categories/ChoicesTemplate.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import {
-  BottomNavigation, BottomNavigationAction,
-  Grid
+  BottomNavigation, BottomNavigationAction, IconButton,
+  Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
+  Grid, Paper,
+
 } from '@material-ui/core';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CancelIcon from '@material-ui/icons/Cancel';
+import EditIcon from '@material-ui/icons/Edit';
 
 export const ChoicesTemplate = (): JSX.Element => {
+  const templates = [
+    ['hoge', 'foo', 'var']
+  ];
+
   /**
    * Add new template set.
    */
@@ -19,6 +27,36 @@ export const ChoicesTemplate = (): JSX.Element => {
         <h1>選択肢テンプレート</h1>
       </Grid>
       <Grid item xs={12}>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell />
+                <TableCell>選択肢</TableCell>
+                <TableCell />
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {templates.map((choices: string[], index: number) =>
+                <TableRow key={`template_${index}`}>
+                  <TableCell>
+                    <IconButton size='small'>
+                      <EditIcon />
+                    </IconButton>
+                  </TableCell>
+                  <TableCell>{choices.join(',')}</TableCell>
+                  <TableCell align='right'>
+                    <IconButton size='small'>
+                      <CancelIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+      <Grid item xs={12}>
         <BottomNavigation showLabels>
           <BottomNavigationAction
             label="テンプレート追加"
@@ -27,8 +65,7 @@ export const ChoicesTemplate = (): JSX.Element => {
           />
         </BottomNavigation>
       </Grid>
-
-    </Grid>
+    </Grid >
   );
 }
 ChoicesTemplate.displayName = ChoicesTemplate.name;
