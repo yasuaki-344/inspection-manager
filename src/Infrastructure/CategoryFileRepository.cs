460ef1856d13db1ac90d522cef1d62675d228269
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-27 17:43:37 +0900

    Implement template control

--- src/Infrastructure/CategoryFileRepository.cs
+++ src/Infrastructure/CategoryFileRepository.cs
@@ -4,8 +4,10 @@
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 //
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using InspectionManager.ApplicationCore.Dto;
 using InspectionManager.ApplicationCore.Interfaces;
 
 namespace InspectionManager.Infrastructure
@@ -15,13 +17,13 @@ namespace InspectionManager.Infrastructure
         private readonly string _baseDirectory = "categories";
         private readonly string _inspectionGroupsFile = "inspection-group.json";
         private readonly string _inspectionTypesFile = "inspection-type.json";
+        private readonly string _choiceTemplateFile = "choice-template.json";
 
         /// <summary>
         /// Initializes a new instance of CategoryFileRepository class.
         /// </summary>
         public CategoryFileRepository()
-        {
-        }
+        { }
 
         /// <inheritdoc/>
         public string[] GetInspectionGroups()
@@ -83,6 +85,40 @@ namespace InspectionManager.Infrastructure
             var json = JsonSerializer.Serialize(groups);
             File.WriteAllText(filePath, json);
             return groups;
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<ChoiceTemplateDto> GetChoiceTemplates()
+        {
+            if (!Directory.Exists(_baseDirectory))
+            {
+                throw new DirectoryNotFoundException(_baseDirectory);
+            }
+            var filePath = Path.Join(_baseDirectory, _choiceTemplateFile);
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(filePath);
+            }
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<IEnumerable<ChoiceTemplateDto>>(json)
+            ?? new List<ChoiceTemplateDto>();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<ChoiceTemplateDto> CreateChoiceTemplates(
+            IEnumerable<ChoiceTemplateDto> templates
+        )
+        {
+            if (!Directory.Exists(_baseDirectory))
+            {
+                Directory.CreateDirectory(_baseDirectory);
+            }
+
+            var filePath = Path.Join(_baseDirectory, _choiceTemplateFile);
+
+            var json = JsonSerializer.Serialize(templates);
+            File.WriteAllText(filePath, json);
+            return templates;
         }
 
     }
