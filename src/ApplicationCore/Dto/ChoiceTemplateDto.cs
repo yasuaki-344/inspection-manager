3f3076098ee21fe4e27347eedcf7da7828fcfa1b
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-27 17:32:52 +0900

    Create ChoiceTemplateDto

---
+++ src/ApplicationCore/Dto/ChoiceTemplateDto.cs
@@ -0,0 +1,17 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace InspectionManager.ApplicationCore.Dto
+{
+    public class ChoiceTemplateDto
+    {
+        [JsonPropertyName("choices")]
+        public List<string> Choices { get; set; } = new List<string>();
+    }
+}
