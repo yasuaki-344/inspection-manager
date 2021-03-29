01eb2ff922950bf457ec3215af88b2a479c01271
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-30 00:49:08 +0900

    Add unit test for Home

---
+++ src/Web/ClientApp/src/test/Home.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
+import { Home } from '../components/Home';
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders without crashing', async () => {
+  await act(async () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+      , container
+    );
+  });
+});
