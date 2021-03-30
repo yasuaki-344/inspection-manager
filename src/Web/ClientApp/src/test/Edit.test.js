dc0ac558b100ab0a22099ff0c7645fdc3dda7cc2
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-30 11:09:04 +0900

    Add unit test for Edit

---
+++ src/Web/ClientApp/src/test/Edit.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
+import { Edit } from '../inspection/Edit';
+
+jest.mock('../inspection/InspectionSheetForm', () => {
+  return {
+    InspectionSheetForm: (props) => {
+      return <></>
+    },
+  };
+});
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders without crashing', async () => {
+  await act(async () => {
+    render(
+      <MemoryRouter>
+        <Edit
+          match={{ params: { id: 'guid' } }}
+        />
+      </MemoryRouter>
+      , container
+    );
+  });
+});
