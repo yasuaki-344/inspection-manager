bc79d89a81a21a24239b04af709d6064c4e14f76
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-03-30 11:14:32 +0900

    Modify unit test for App

--- src/Web/ClientApp/src/test/App.test.js
+++ src/Web/ClientApp/src/test/App.test.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
+jest.mock('../components/Home', () => {
+  return { Home: () => { return <></> }, };
+});
+jest.mock('../inspection/Create', () => {
+  return { Create: () => { return <></> }, };
+});
+jest.mock('../inspection/Details', () => {
+  return { Details: () => { return <></> }, };
+});
+jest.mock('../inspection/Edit', () => {
+  return { Edit: () => { return <></> }, };
+});
+jest.mock('../categories/InspectionGroupCategory', () => {
+  return { InspectionGroupCategory: () => { return <></> }, };
+});
+jest.mock('../categories/InspectionTypeCategory', () => {
+  return { InspectionTypeCategory: () => { return <></> }, };
+});
+jest.mock('../categories/ChoicesTemplate', () => {
+  return { ChoicesTemplate: () => { return <></> }, };
+});
+
 it('renders without crashing', async () => {
   const div = document.createElement('div');
   ReactDOM.render(
