85b58df149a2ca7f2a78f1c2830ca425bad7f2a6
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-04-04 10:02:24 +0900

    Add test page for drag and drop

--- src/Web/ClientApp/src/Experiment.jsx
+++ src/Web/ClientApp/src/Experiment.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 export const Experiment = () => {
   return (
-    <div></div>
+    <div>
+       <List component="nav" aria-label="main mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Inbox" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Drafts" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Spam" />
+          </ListItem>
+      </List>
+    </div>
   )
 }
