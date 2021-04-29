86575d671979343073938a2779f72c9a6e0c6bb6
yasuaki-344 <ikausay.443@gmail.com>
2021-04-29 21:35:34 +0900

    Update unit test for ChoiceTemplate

--- src/Web/ClientApp/src/categories/__tests__/ChoicesTemplate.test.js
+++ src/Web/ClientApp/src/categories/__tests__/ChoicesTemplate.test.js
@@ -1,41 +1,93 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { ChoicesTemplate } from '../ChoicesTemplate';
 
-let container = null;
 beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
+  jest.spyOn(global, 'fetch').mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve([
+        { choices: ['choice1-1', 'choice1-2'] },
+        { choices: ['choice2-1', 'choice2-2'] },
+        { choices: ['choice3-1', 'choice3-2'] },
+      ])
+    })
+  );
+  jest.spyOn(window, 'alert').mockImplementation(() => { });
 });
-
 afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
+  jest.resetAllMocks();
 });
 
 it('renders without crashing', async () => {
-  const templates = [
-    { choices: ['choice1-1', 'choice1-2'] },
-    { choices: ['choice2-1', 'choice2-2'] },
-    { choices: ['choice3-1', 'choice3-2'] },
-  ];
-  jest.spyOn(global, 'fetch').mockImplementation(() =>
-    Promise.resolve({
-      json: () => Promise.resolve(templates)
-    })
-  );
-
   await act(async () => {
     render(
       <MemoryRouter>
         <ChoicesTemplate />
       </MemoryRouter>
-      , container
     );
   });
+});
 
-  global.fetch.mockRestore();
+it('click add template button', async () => {
+  await act(async () => {
+    render(
+      <MemoryRouter>
+        <ChoicesTemplate />
+      </MemoryRouter>
+    );
+  });
+  fireEvent.click(screen.getByTestId('add-template-button'));
+  fireEvent.click(screen.getByTestId('add-choice-button'));
+  fireEvent.change(
+    screen.getByDisplayValue(''),
+    { target: { value: 'new choice' } }
+  );
+  fireEvent.change(
+    screen.getByDisplayValue('new choice'),
+    { target: { value: 'new choice1' } }
+  );
+  fireEvent.click(screen.getByTestId('add-choice-button'));
+  fireEvent.change(
+    screen.getByDisplayValue(''),
+    { target: { value: 'new choice2' } }
+  );
+  fireEvent.click(screen.getByTestId('remove-choice-0'));
+  fireEvent.click(screen.getByText(/ok/i));
+});
+
+it('click edit template button', async () => {
+  await act(async () => {
+    render(
+      <MemoryRouter>
+        <ChoicesTemplate />
+      </MemoryRouter>
+    );
+  });
+  fireEvent.click(screen.getByTestId('edit-template-button-0'));
+  fireEvent.click(screen.getByText(/キャンセル/i));
+});
+
+it('click remove template button', async () => {
+  await act(async () => {
+    render(
+      <MemoryRouter>
+        <ChoicesTemplate />
+      </MemoryRouter>
+    );
+  });
+  fireEvent.click(screen.getByTestId('remove-template-button-0'));
+});
+
+
+it('click submit type button', async () => {
+  await act(async () => {
+    render(
+      <MemoryRouter>
+        <ChoicesTemplate />
+      </MemoryRouter>
+    );
+    fireEvent.submit(screen.getByTestId('form'));
+  });
 });
