010358360e795a8eb673766b454a7298a1c76080
yasuaki-344 <ikausay.443@gmail.com>
2021-04-29 21:52:17 +0900

    Update unit test for ChoiceSetSelectDialog

--- src/Web/ClientApp/src/inspection/dialog/__tests__/ChoiceSetSelectDialog.test.js
+++ src/Web/ClientApp/src/inspection/dialog/__tests__/ChoiceSetSelectDialog.test.js
@@ -1,23 +1,57 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { ChoiceSetSelectDialog } from '../ChoiceSetSelectDialog';
 
-it('renders without crashing', () => {
-  const templates = [
-    { choices: ['choice1-1', 'choice1-2'] },
-    { choices: ['choice2-1', 'choice2-2'] },
-    { choices: ['choice3-1', 'choice3-2'] },
-  ];
+beforeEach(() => {
   jest.spyOn(global, 'fetch').mockImplementation(() =>
     Promise.resolve({
-      json: () => Promise.resolve(templates)
+      json: () => Promise.resolve([
+        { choices: ['choice1-1', 'choice1-2'] },
+        { choices: ['choice2-1', 'choice2-2'] },
+        { choices: ['choice3-1', 'choice3-2'] },
+      ])
     })
   );
+  jest.spyOn(window, 'alert').mockImplementation(() => { });
+});
+afterEach(() => {
+  jest.resetAllMocks();
+});
 
-  render(
-    <ChoiceSetSelectDialog
-      open={true}
-      handleClose={() => { }}
-    />
-  );
+it('renders without crashing', async () => {
+  await act(async () => {
+    render(
+      <ChoiceSetSelectDialog
+        open={true}
+        handleClose={() => { }}
+      />
+    );
+  });
+});
+
+it('select template', async () => {
+  await act(async () => {
+    render(
+      <ChoiceSetSelectDialog
+        open={true}
+        handleClose={() => { }}
+      />
+    );
+  });
+  fireEvent.click(screen.getByTestId('radio-0'));
+  fireEvent.click(screen.getByTestId('radio-1'));
+  fireEvent.click(screen.getByText(/ok/i));
+});
+
+it('click cancel button', async () => {
+  await act(async () => {
+    render(
+      <ChoiceSetSelectDialog
+        open={true}
+        handleClose={() => { }}
+      />
+    );
+  });
+  fireEvent.click(screen.getByText(/キャンセル/i));
 });
