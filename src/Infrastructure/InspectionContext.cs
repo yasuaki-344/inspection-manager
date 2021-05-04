11d4029aaf97be6888b915deb77dd762337c14c5
yasuaki-344 <ikausay.443@gmail.com>
2021-05-04 12:10:52 +0900

    Create InspectionDbContext.cs

---
+++ src/Infrastructure/InspectionContext.cs
@@ -0,0 +1,18 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+using Microsoft.EntityFrameworkCore;
+
+namespace InspectionManager.Infrastructure
+{
+    public class InspectionContext : DbContext
+    {
+        public InspectionContext(DbContextOptions<InspectionContext> options) : base(options)
+        {
+
+        }
+    }
+}
