f87a03bd9f51558d1b06ac75c6a02cd68a3300db
yasuaki-344 <ikausay.443@gmail.com>
2021-05-06 15:32:02 +0900

    Create InspectionItem entity

---
+++ src/ApplicationCore/Entities/InspectionItem.cs
@@ -0,0 +1,22 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+using System.Collections.Generic;
+
+namespace InspectionManager.ApplicationCore.Entities
+{
+    /// <summary>
+    /// Represents inspection item entity
+    /// </summary>
+    public class InspectionItem
+    {
+        public int InspectionItemId { get; set; }
+        public int OrderIndex { get; set; }
+        public string InspectionContent { get; set; } = string.Empty;
+        public InputType InputType { get; set; } = new InputType();
+        public List<Choice> Choices { get; set; } = new List<Choice>();
+    }
+}
