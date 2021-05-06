a2ac84b509dbf1dee699dbb189e395a38e9f911a
yasuaki-344 <ikausay.443@gmail.com>
2021-05-06 15:32:24 +0900

    Create Equipment entity

---
+++ src/ApplicationCore/Entities/Equipment.cs
@@ -0,0 +1,21 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+using System.Collections.Generic;
+
+namespace InspectionManager.ApplicationCore.Entities
+{
+    /// <summary>
+    /// Represents equipment entity
+    /// </summary>
+    public class Equipment
+    {
+        public int EquipmentId { get; set; }
+        public int OrderIndex { get; set; }
+        public string EquipmentName { get; set; } = string.Empty;
+        public List<InspectionItem> InspectionItems { get; set; } = new List<InspectionItem>();
+    }
+}
