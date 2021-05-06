5711a5243ee1ccf4e8e732442fcf56181c0fb30c
yasuaki-344 <ikausay.443@gmail.com>
2021-05-06 15:31:45 +0900

    Create Option entity

---
+++ src/ApplicationCore/Entities/Option.cs
@@ -0,0 +1,17 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+namespace InspectionManager.ApplicationCore.Entities
+{
+    /// <summary>
+    /// Represents option entity
+    /// </summary>
+    public class Option
+    {
+        public int OptionId { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
