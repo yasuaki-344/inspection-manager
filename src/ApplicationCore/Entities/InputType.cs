475df7f9b6c5dbea3ebfe501f505622b73c55a6b
yasuaki-344 <ikausay.443@gmail.com>
2021-05-06 15:32:13 +0900

    Create InputType entity

---
+++ src/ApplicationCore/Entities/InputType.cs
@@ -0,0 +1,17 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+namespace InspectionManager.ApplicationCore.Entities
+{
+    /// <summary>
+    /// Represents input type entity
+    /// </summary>
+    public class InputType
+    {
+        public int InputTypeId { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
