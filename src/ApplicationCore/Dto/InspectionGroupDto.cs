f95c816023bd3795b4202ad5ce90dfac63ac7ce0
yasuaki-344 <ikausay.443@gmail.com>
2021-05-08 23:42:09 +0900

    Add json property name to dto

--- src/ApplicationCore/Dto/InspectionGroupDto.cs
+++ src/ApplicationCore/Dto/InspectionGroupDto.cs
@@ -4,12 +4,16 @@
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 //
+using System.Text.Json.Serialization;
 
 namespace InspectionManager.ApplicationCore.Dto
 {
     public class InspectionGroupDto
     {
+        [JsonPropertyName("inspection_group_id")]
         public int InspectionGroupId { get; set; }
+
+        [JsonPropertyName("description")]
         public string Description { get; set; } = string.Empty;
     }
 }
