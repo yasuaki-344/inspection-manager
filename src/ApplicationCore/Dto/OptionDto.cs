6f78528075902e4c0bb4624f74e182aaaed0adfe
yasuaki-344 <ikausay.443@gmail.com>
2021-05-19 22:08:45 +0900

    Create OptionDto.cs

---
+++ src/ApplicationCore/Dto/OptionDto.cs
@@ -0,0 +1,19 @@
+﻿//
+// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//
+using System.Text.Json.Serialization;
+
+namespace InspectionManager.ApplicationCore.Entities
+{
+    public class OptionDto
+    {
+        [JsonPropertyName("option_id")]
+        public int OptionId { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
