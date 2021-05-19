f4b809153cf5ef7c1c66a1366ccdd011953908d8
yasuaki-344 <ikausay.443@gmail.com>
2021-05-19 22:11:21 +0900

    Modify namespace

--- src/ApplicationCore/Dto/OptionDto.cs
+++ src/ApplicationCore/Dto/OptionDto.cs
@@ -6,7 +6,7 @@
 //
 using System.Text.Json.Serialization;
 
-namespace InspectionManager.ApplicationCore.Entities
+namespace InspectionManager.ApplicationCore.Dto
 {
     public class OptionDto
     {
