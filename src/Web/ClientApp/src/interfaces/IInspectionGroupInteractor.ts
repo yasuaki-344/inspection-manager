c78f937acbfa99aa188f8dcc6868227f027f6ed9
yasuaki-344 <ikausay.443@gmail.com>
2021-09-11 17:15:31 +0900

    Implement interactor

--- src/Web/ClientApp/src/interfaces/IInspectionGroupInteractor.ts
+++ src/Web/ClientApp/src/interfaces/IInspectionGroupInteractor.ts
@@ -1,3 +1,10 @@
-export interface IInspectionGroupInteractor {
+import { InspectionGroup } from "../typescript-fetch";
 
+export interface IInspectionGroupInteractor {
+  getGroups(): Array<InspectionGroup>
+  get(): void
+  getById(id: number): InspectionGroup | undefined
+  create(inspectionGroup: InspectionGroup): Promise<void>
+  update(inspectionGroup: InspectionGroup): Promise<void>
+  delete(id: number): Promise<void>
 }
--- src/Web/ClientApp/src/use-cases/InspectionGroupInteractor.ts
+++ src/Web/ClientApp/src/use-cases/InspectionGroupInteractor.ts
@@ -15,4 +15,38 @@ export class InspectionGroupInteractor implements IInspectionGroupInteractor {
     this.setGroups = setGroups;
     this.repository = repository;
   }
+
+  getGroups(): Array<InspectionGroup> {
+    return this.groups;
+  }
+
+  get(): void {
+    this.repository.get()
+      .then(res => {
+        this.setGroups(res)
+      })
+      .catch(console.error);
+  }
+
+  getById(id: number): InspectionGroup | undefined {
+    return this.groups.find(x => x.inspection_group_id === id);
+  }
+
+  async create(inspectionGroup: InspectionGroup): Promise<void> {
+    const res = await this.repository.post(inspectionGroup);
+    this.setGroups(this.groups.concat(res));
+  }
+
+  async update(inspectionGroup: InspectionGroup): Promise<void> {
+    const res = await this.repository.put(inspectionGroup);
+    this.setGroups(this.groups.map(x =>
+      (x.inspection_group_id === res.inspection_group_id) ? res : x
+    ));
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.repository.delete(id);
+    this.setGroups(this.groups.filter((x: InspectionGroup) =>
+      x.inspection_group_id !== id));
+  }
 }
