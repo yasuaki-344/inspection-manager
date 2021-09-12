7dd5e54a0f7e637431e84be815eb07c9100b455f
yasuaki-344 <ikausay.443@gmail.com>
2021-09-12 20:40:01 +0900

    Extract styles

--- src/Web/ClientApp/src/components/inspection/form/InspectionSheetForm.tsx
+++ src/Web/ClientApp/src/components/inspection/form/InspectionSheetForm.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import {
   BottomNavigation, BottomNavigationAction, Grid, Paper
 } from '@material-ui/core';
@@ -12,25 +11,6 @@ import { InspectionItem, InspectionSheet } from '../../../entities';
 import { InspectionGroup, InspectionType } from '../../../typescript-fetch';
 import { BottomNavigationAdd } from '../../common';
 import { InspectionGroupRepository, InspectionTypeRepository } from '../../../infrastructure';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    sheetLabel: {
-      backgroundColor: theme.palette.primary.main,
-      color: "#FFFFFF",
-      fontSize: 24,
-      padding: 4,
-    },
-    sheetElement: {
-      margin: 4,
-      width: 250,
-    },
-    sheetIdElement: {
-      margin: 4,
-      width: 330,
-    },
-  })
-);
 
 interface InspectionSheetFormProps {
   isEdit: boolean,
---
+++ src/Web/ClientApp/src/components/stylesheets/App.ts
@@ -0,0 +1,20 @@
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    sheetLabel: {
+      backgroundColor: theme.palette.primary.main,
+      color: "#FFFFFF",
+      fontSize: 24,
+      padding: 4,
+    },
+    sheetElement: {
+      margin: 4,
+      width: 250,
+    },
+    sheetIdElement: {
+      margin: 4,
+      width: 330,
+    },
+  })
+);
