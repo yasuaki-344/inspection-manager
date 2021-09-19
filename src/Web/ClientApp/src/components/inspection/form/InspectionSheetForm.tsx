f8f9727536e8d720fb37e64b714429c0785a0e7d
yasuaki-344 <ikausay.443@gmail.com>
2021-09-19 22:55:22 +0900

    Modify form design

--- src/Web/ClientApp/src/components/inspection/form/InspectionSheetForm.tsx
+++ src/Web/ClientApp/src/components/inspection/form/InspectionSheetForm.tsx
@@ -9,8 +9,8 @@ import { InspectionItemDialog } from '../dialog/InspectionItemDialog';
 import { InspectionSheetContext, InspectionItemContext } from '../../../App';
 import { InspectionItem, InspectionSheet } from '../../../entities';
 import { InspectionGroup, InspectionType } from '../../../typescript-fetch';
-import { BottomNavigationAdd } from '../../common';
 import { InspectionGroupRepository, InspectionTypeRepository } from '../../../infrastructure';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 interface InspectionSheetFormProps {
   isEdit: boolean,
@@ -102,11 +102,12 @@ export const InspectionSheetForm: FC<InspectionSheetFormProps> = ({ isEdit }): J
                 icon={<UndoIcon />}
                 onClick={getHistory}
               />
+              <BottomNavigationAction
+                label="点検機器追加"
+                icon={<AddCircleIcon />}
+                onClick={() => sheetController.addEquipment()}
+              />
             </BottomNavigation>
-            <BottomNavigationAdd
-              label="点検機器追加"
-              onClick={() => sheetController.addEquipment()}
-            />
           </Grid>
         </Grid>
       </Paper >
