1e6d6cf7af8b51a1e5d1d217603238b4ea2f790d
yasuaki-344 <ikausay.443@gmail.com>
2021-09-19 16:08:38 +0900

    Update Edit component

--- src/Web/ClientApp/src/components/inspection/Edit.tsx
+++ src/Web/ClientApp/src/components/inspection/Edit.tsx
@@ -1,22 +1,20 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Alert } from '@mui/material';
 import { Button, Grid } from '@mui/material';
 import { InspectionSheetForm } from './form/InspectionSheetForm';
 import { InspectionSheetContext } from '../../App';
 import { TopPageLink } from '../common';
+import { Notification, NotificationInitState, NotificationStateInteractor } from '../common/Notification';
 
 export const Edit = ({ match }: any): JSX.Element => {
   const sheetId = match.params.id;
   const { sheetController } = useContext(InspectionSheetContext);
-  const [successMessage, setSuccessMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const notification = new NotificationStateInteractor(useState(NotificationInitState));
 
   useEffect(() => {
     sheetController.getInspectionSheetById(sheetId)
       .catch((error) => {
-        setSuccessMessage('');
-        setErrorMessage(`データの取得に失敗しました (ID:${sheetId})`);
         console.error(error);
+        notification.setMessageState('error', `データの取得に失敗しました (ID:${sheetId})`);
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sheetId]);
@@ -25,13 +23,11 @@ export const Edit = ({ match }: any): JSX.Element => {
     event.preventDefault();
     sheetController.updateInspectionSheet()
       .then(() => {
-        setSuccessMessage('更新に成功しました');
-        setErrorMessage('');
+        notification.setMessageState('success', '更新に成功しました');
       })
       .catch(error => {
         console.error(error)
-        setSuccessMessage('');
-        setErrorMessage('更新に失敗しました');
+        notification.setMessageState('error', '更新に失敗しました');
       });
   }
 
@@ -43,20 +39,14 @@ export const Edit = ({ match }: any): JSX.Element => {
       <Grid item xs={12}>
         <TopPageLink />
       </Grid>
-      {errorMessage !== '' &&
-        <Grid item xs={12}>
-          <Alert elevation={6} variant="filled" severity="error">
-            {errorMessage}
-          </Alert>
-        </Grid>
-      }
-      {successMessage !== '' &&
-        <Grid item xs={12}>
-          <Alert elevation={6} variant="filled" severity="success">
-            {successMessage}
-          </Alert>
-        </Grid>
-      }
+      <Grid item xs={12}>
+        <Notification
+          open={notification.state.isOpen}
+          severity={notification.state.severity}
+          message={notification.state.message}
+          onClose={() => { notification.hideDisplay() }}
+        />
+      </Grid>
       <Grid item xs={12}>
         <form data-testid='form' onSubmit={handleUpdate}>
           <Grid container spacing={1}>
