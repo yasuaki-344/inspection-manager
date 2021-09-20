7aee476c98120e9e0966c2a8b0b609bce0bef431
yasuaki-344 <ikausay.443@gmail.com>
2021-09-20 20:28:29 +0900

    Modify entities

--- src/Web/ClientApp/src/entities/Equipment.ts
+++ src/Web/ClientApp/src/entities/Equipment.ts
@@ -1,7 +1,10 @@
 import { InspectionItem } from ".";
 
 export type Equipment = {
+  // eslint-disable-next-line
   equipment_id: number;
+  // eslint-disable-next-line
   equipment_name: string;
+  // eslint-disable-next-line
   inspection_items: InspectionItem[];
 };
