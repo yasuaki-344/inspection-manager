e209bd27b26194c573c8e7bc05f910eb235dbcd5
yasuaki-344 <ikausay.443@gmail.com>
2021-11-06 17:44:59 +0900

    Update controller unit test

--- src/Web/ClientApp/src/controllers/__tests__/InspectionGroupController.test.js
+++ src/Web/ClientApp/src/controllers/__tests__/InspectionGroupController.test.js
@@ -1,7 +1,7 @@
 import { InspectionGroupController } from "..";
 
 describe("InspectionGroupController unit test", () => {
-  test("fetch inspection groups correctly", async () => {
+  test("Fetch inspection groups correctly", async () => {
     const useCase = {
       fetchInspectionGroup: jest.fn(async () => {}),
     };
@@ -10,9 +10,60 @@ describe("InspectionGroupController unit test", () => {
     expect(useCase.fetchInspectionGroup).toHaveBeenCalledTimes(1);
   });
 
-  test("delete inspection groups correctly", async () => {
+  test("Create edit item correctly", () => {
     const useCase = {
-      delete: jest.fn(async (id) => {}),
+      createEditItem: jest.fn(async () => {}),
+    };
+    const target = new InspectionGroupController(useCase);
+    target.createEditItem();
+    expect(useCase.createEditItem).toHaveBeenCalled();
+  });
+
+  test("Set edit item correctly", () => {
+    const useCase = {
+      setEditItem: jest.fn(async () => {}),
+    };
+    const target = new InspectionGroupController(useCase);
+    target.setEditItem(10);
+    expect(useCase.setEditItem).toHaveBeenCalledWith(10);
+  });
+
+  test("Set edit item correctly", () => {
+    const useCase = {
+      editGroup: jest.fn(async () => {}),
+    };
+    const target = new InspectionGroupController(useCase);
+    target.editGroup({ target: { name: "description", value: "group" } });
+    expect(useCase.editGroup).toHaveBeenCalledWith("description", "group");
+  });
+
+  test("Create inspection group correctly", async () => {
+    const useCase = {
+      create: jest.fn(async () => {}),
+    };
+    const target = new InspectionGroupController(useCase);
+    await target.create({ inspectionGroupId: 1, description: "group" });
+    expect(useCase.create).toHaveBeenCalledWith({
+      inspectionGroupId: 1,
+      description: "group",
+    });
+  });
+
+  test("Update inspection group correctly", async () => {
+    const useCase = {
+      update: jest.fn(async () => {}),
+    };
+    const target = new InspectionGroupController(useCase);
+    await target.update({ inspectionGroupId: 1, description: "group" });
+    expect(useCase.update).toHaveBeenCalledWith({
+      inspectionGroupId: 1,
+      description: "group",
+    });
+  });
+
+  test("Delete inspection groups correctly", async () => {
+    const useCase = {
+      delete: jest.fn(async () => {}),
     };
     const target = new InspectionGroupController(useCase);
     await target.delete(10);
