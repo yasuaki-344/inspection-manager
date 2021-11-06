e5be6afa152d959fbd06d238da00bfb70c2b6993
yasuaki-344 <ikausay.443@gmail.com>
2021-11-06 15:51:21 +0900

    Add unit test for JsonKeyNameConverter

--- src/Web/ClientApp/src/controllers/__tests__/HomeController.test.js
+++ src/Web/ClientApp/src/controllers/__tests__/HomeController.test.js
@@ -3,11 +3,11 @@ import { HomeController } from "..";
 describe("HomeController unit test", () => {
   test("fetch display data correctly", async () => {
     const typeUseCase = {
-      fetchInspectionTypes: jest.fn(async () => { })
-    }
+      fetchInspectionTypes: jest.fn(async () => {}),
+    };
     const groupUseCase = {
-      fetchInspectionGroup: jest.fn(async () => { })
-    }
+      fetchInspectionGroup: jest.fn(async () => {}),
+    };
     const target = new HomeController(typeUseCase, groupUseCase);
     await target.fetchDisplayData();
     expect(typeUseCase.fetchInspectionTypes).toHaveBeenCalledTimes(1);
--- src/Web/ClientApp/src/presenters/__tests__/HomePresenter.test.js
+++ src/Web/ClientApp/src/presenters/__tests__/HomePresenter.test.js
@@ -2,20 +2,20 @@ import { HomePresenter } from "..";
 
 describe("HomePresenter unit test", () => {
   test("Get group IDs correctly", async () => {
-    const typeUseCase = {}
+    const typeUseCase = {};
     const groupUseCase = {
-      getIds: jest.fn((keyword) => [1, 2, 3])
-    }
+      getIds: jest.fn((keyword) => [1, 2, 3]),
+    };
     const target = new HomePresenter(typeUseCase, groupUseCase);
     const actual = target.getGroupIds("keyword");
     expect(actual).toEqual(expect.arrayContaining([1, 2, 3]));
   });
 
   test("Get group name correctly", async () => {
-    const typeUseCase = {}
+    const typeUseCase = {};
     const groupUseCase = {
-      getName: jest.fn((id) => "test")
-    }
+      getName: jest.fn((id) => "test"),
+    };
     const target = new HomePresenter(typeUseCase, groupUseCase);
     const actual = target.getGroupName(1);
     expect(actual).toEqual("test");
