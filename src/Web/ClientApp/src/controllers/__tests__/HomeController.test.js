85a388cb423942a67a82968739a5ac13c5b1c644
yasuaki-344 <ikausay.443@gmail.com>
2021-11-06 15:38:58 +0900

    Add unit test for HomeController

---
+++ src/Web/ClientApp/src/controllers/__tests__/HomeController.test.js
@@ -0,0 +1,16 @@
+import { HomeController } from "..";
+
+describe("HomeController unit test", () => {
+  test("fetch display data correctly", async () => {
+    const typeUseCase = {
+      fetchInspectionTypes: jest.fn(async () => { })
+    }
+    const groupUseCase = {
+      fetchInspectionGroup: jest.fn(async () => { })
+    }
+    const target = new HomeController(typeUseCase, groupUseCase);
+    await target.fetchDisplayData();
+    expect(typeUseCase.fetchInspectionTypes).toHaveBeenCalledTimes(1);
+    expect(groupUseCase.fetchInspectionGroup).toHaveBeenCalledTimes(1);
+  });
+});
