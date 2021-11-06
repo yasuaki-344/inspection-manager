d84b5662d39fd888319186358eb8488e08df290f
yasuaki-344 <ikausay.443@gmail.com>
2021-11-06 15:30:26 +0900

    Add unit test for InspectionGroupInteractor

---
+++ src/Web/ClientApp/src/use-cases/__tests__/InspectionGroupInteractor.test.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { InspectionGroupInteractor } from "..";
+
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useState: jest.fn(),
+}));
+
+describe("InspectionGroupInteractor unit test", () => {
+  const setState = jest.fn();
+
+  beforeEach(() => {
+    useState.mockImplementation((init) => [init, setState]);
+  });
+
+  test("fetch inspection group correctly", async () => {
+    const repository = {
+      get: jest.fn(async () => []),
+    };
+    const target = new InspectionGroupInteractor(repository);
+    await target.fetchInspectionGroup();
+    expect(setState).toHaveBeenCalledTimes(1);
+  });
+});
