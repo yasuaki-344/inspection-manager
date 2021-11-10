da4ce6bb320a26ab09cadf1dd104fadebc92e919
yasuaki-344 <ikausay.443@gmail.com>
2021-11-10 21:37:42 +0900

    Update open api

--- src/Web/ClientApp/src/typescript-fetch/apis/index.ts
+++ src/Web/ClientApp/src/typescript-fetch/apis/index.ts
@@ -1,3 +1,4 @@
-export * from "./ChoiceTemplatesApi";
-export * from "./InspectionGroupsApi";
-export * from "./InspectionTypesApi";
+export * from './ChoiceTemplatesApi';
+export * from './InspectionGroupsApi';
+export * from './InspectionSheetsApi';
+export * from './InspectionTypesApi';
