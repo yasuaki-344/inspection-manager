99f6dc86c1f6885b8852d64227d82ebfeff03935
yasuaki-344 <ikausay.443@gmail.com>
2021-11-11 21:05:11 +0900

    Create InspectionSheetDetailDto

---
+++ src/ApplicationCore/Dto/InspectionSheetDetailDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace InspectionManager.ApplicationCore.Dto
+{
+    public class InspectionSheetDetailDto
+    {
+        [JsonPropertyName("sheet_id")]
+        public int SheetId { get; set; }
+
+        [JsonPropertyName("sheet_name")]
+        public string SheetName { get; set; } = string.Empty;
+
+        [JsonPropertyName("inspection_type_id")]
+        public int InspectionTypeId { get; set; }
+
+        [JsonPropertyName("inspection_group_id")]
+        public int InspectionGroupId { get; set; }
+
+        [JsonPropertyName("equipments")]
+        public ICollection<EquipmentDto> Equipments { get; set; } = new List<EquipmentDto>();
+    }
+}
