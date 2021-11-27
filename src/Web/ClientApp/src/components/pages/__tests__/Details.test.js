8be6033024d018e0c0fdc4a6b011976959cfe1ee
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-11-27 10:48:05 +0900

    Modify Details component unit test

--- src/Web/ClientApp/src/components/pages/__tests__/Details.test.js
+++ src/Web/ClientApp/src/components/pages/__tests__/Details.test.js
@@ -1,47 +1,27 @@
 import React from "react";
-import { act } from "react-dom/test-utils";
+import ReactDOM from "react-dom";
 import { MemoryRouter } from "react-router-dom";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { DIContainerContext } from "../../../container";
 import { Details } from "../Details";
 
-beforeEach(() => {
-  jest.spyOn(global, "fetch").mockImplementation(() =>
-    Promise.resolve({
-      json: () =>
-        Promise.resolve({
-          sheet_id: "sheet id",
-          sheet_name: "sheet name",
-          inspection_group: "group",
-          inspection_type: "type",
-          equipments: [
-            {
-              equipment_id: "equipment id",
-              equipment_name: "equipment name",
-              inspection_items: [
-                {
-                  inspection_item_id: "inspection item id",
-                  inspection_content: "inspection content",
-                  input_type: 0,
-                  choices: ["choice1", "choice2"],
-                },
-              ],
-            },
-          ],
-        }),
-    })
-  );
-  jest.spyOn(window, "alert").mockImplementation(() => {});
-});
-afterEach(() => {
-  jest.resetAllMocks();
-});
+describe("Details compoenent unite test", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    const container = {
+      IDetailController: {},
+      IDetailPresenter: {
+        getInspectionGroup: jest.fn(() => {}),
+        getInspectionType: jest.fn(() => {}),
+      },
+    };
 
-it("renders without crashing", async () => {
-  await act(async () => {
-    render(
-      <MemoryRouter>
-        <Details match={{ params: { id: "guid" } }} />
-      </MemoryRouter>
+    ReactDOM.render(
+      <DIContainerContext.Provider value={container}>
+        <MemoryRouter>
+          <Details match={{ params: { id: 1 } }} />
+        </MemoryRouter>
+      </DIContainerContext.Provider>,
+      div
     );
   });
 });
