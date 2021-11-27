88478924b9edcfa0dfb95a1752343da5cb085148
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-11-27 10:59:51 +0900

    Modify Edit compoent unit test

--- src/Web/ClientApp/src/components/pages/__tests__/Edit.test.js
+++ src/Web/ClientApp/src/components/pages/__tests__/Edit.test.js
@@ -1,46 +1,24 @@
 import React from "react";
-import { act } from "react-dom/test-utils";
+import ReactDOM from "react-dom";
 import { MemoryRouter } from "react-router-dom";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { DIContainerContext } from "../../../container";
 import { Edit } from "../Edit";
 
-jest.mock("../form/InspectionSheetForm", () => {
-  return {
-    InspectionSheetForm: (props) => {
-      return <></>;
-    },
-  };
-});
+describe("Edit compoenent unit test", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    const container = {
+      IInspectionSheetController: {},
+      IInspectionSheetPresenter: {},
+    };
 
-beforeEach(() => {
-  jest.spyOn(global, "fetch").mockImplementation(() =>
-    Promise.resolve({
-      json: () => Promise.resolve([]),
-    })
-  );
-  jest.spyOn(window, "alert").mockImplementation(() => {});
-});
-afterEach(() => {
-  jest.resetAllMocks();
-});
-
-it("renders without crashing", async () => {
-  await act(async () => {
-    render(
-      <MemoryRouter>
-        <Edit match={{ params: { id: "guid" } }} />
-      </MemoryRouter>
+    ReactDOM.render(
+      <DIContainerContext.Provider value={container}>
+        <MemoryRouter>
+          <Edit match={{ params: { id: 2 } }} />
+        </MemoryRouter>
+      </DIContainerContext.Provider>,
+      div
     );
-  });
-});
-
-it("submit", async () => {
-  await act(async () => {
-    render(
-      <MemoryRouter>
-        <Edit match={{ params: { id: "guid" } }} />
-      </MemoryRouter>
-    );
-    fireEvent.submit(screen.getByTestId("form"));
   });
 });
