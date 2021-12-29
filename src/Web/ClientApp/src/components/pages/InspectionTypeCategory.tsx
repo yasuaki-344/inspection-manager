071e2254004497a44eef7f4d1491b0a7e969209a
yasuaki-miyoshi <ikausay.443@gmail.com>
2021-12-29 20:40:17 +0900

    Add new admin settings

--- src/Web/ClientApp/src/components/pages/InspectionTypeCategory.tsx
+++ src/Web/ClientApp/src/components/pages/InspectionTypeCategory.tsx
@@ -2,10 +2,15 @@ import React, { FC } from "react";
 import { Admin, Resource } from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
 import {
-  InspectionTypeList,
-  InspectionTypeEdit,
   InspectionTypeCreate,
+  InspectionTypeEdit,
+  InspectionTypeList,
 } from "./InspectionTypes";
+import {
+  InspectionGroupCreate,
+  InspectionGroupEdit,
+  InspectionGroupList,
+} from "./InspectionGroups";
 
 export const InspectionTypeCategory: FC = (): JSX.Element => {
   return (
@@ -13,13 +18,16 @@ export const InspectionTypeCategory: FC = (): JSX.Element => {
       <Resource
         name="inspection-groups"
         options={{ label: '点検グループ' }}
+        create={InspectionGroupCreate}
+        edit={InspectionGroupEdit}
+        list={InspectionGroupList}
       />
       <Resource
         name="inspection-types"
         options={{ label: '点検タイプ' }}
-        list={InspectionTypeList}
-        edit={InspectionTypeEdit}
         create={InspectionTypeCreate}
+        edit={InspectionTypeEdit}
+        list={InspectionTypeList}
       />
     </Admin>
   );
