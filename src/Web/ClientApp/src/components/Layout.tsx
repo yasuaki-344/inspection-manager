0978cacabb7f3c4f667e69739a4d927375653c99
yasuaki-miyoshi <ikausay.443@gmail.com>
2022-02-05 15:31:27 +0900

    Modify Layout design

--- src/Web/ClientApp/src/components/Layout.tsx
+++ src/Web/ClientApp/src/components/Layout.tsx
@@ -5,11 +5,12 @@ import {
   createTheme,
   CssBaseline,
   ThemeProvider,
+  Toolbar,
 } from "@mui/material";
 import { NavMenu } from "./NavMenu";
 
 interface LayoutProps {
-  children: React.ReactNodeArray | React.ReactNode;
+  children: React.ReactNode;
 }
 
 const mdTheme = createTheme();
@@ -20,7 +21,23 @@ export const Layout: FC<LayoutProps> = (props): JSX.Element => {
       <Box sx={{ display: "flex" }}>
         <CssBaseline />
         <NavMenu />
-        <Container>{props.children}</Container>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            {props.children}
+          </Container>
+        </Box>
       </Box>
     </ThemeProvider>
   );
