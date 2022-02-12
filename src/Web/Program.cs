1b75da46a360e1f89dbe65a14bb15130f6fb3c58
yasuaki-miyoshi <ikausay.443@gmail.com>
2022-02-12 22:53:11 +0900

    Modify server side code

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,42 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using InspectionManager.ApplicationCore.Interfaces;
+using InspectionManager.ApplicationCore.Services;
+using InspectionManager.Infrastructure;
 using InspectionManager.Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
-namespace InspectionManager.Web
+var builder = WebApplication.CreateBuilder(args);
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    DbInitializer.Seed(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
-            host.Run();
-        }
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+    // Auto mapping setting
+    builder.Services.AddAutoMapper(cfg => { cfg.AddProfile<AutoMapping>(); });
+
+    // Database setting
+    if (builder.Environment.IsProduction())
+    {
+        var connectionString = builder.Configuration.GetConnectionString("InspectionContext");
+        builder.Services.AddDbContext<InspectionContext>(options =>
+            options.UseNpgsql(connectionString)
+        );
+    }
+    else if (builder.Environment.IsDevelopment())
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DevelopContext");
+        builder.Services.AddDbContext<InspectionContext>(options =>
+            options.UseSqlite(connectionString)
+        );
+    }
+
+    // Dependency injection setting
+    builder.Services.AddScoped<IInspectionSheetRepository, InspectionSheetRepository>();
+    builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+    builder.Services.AddScoped<IInspectionSheetService, InspectionSheetService>();
+    builder.Services.AddScoped<IExcelDownloadService, ExcelDownloadService>();
+
+    // open API setting
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Open API"
+        });
+    });
+}
+
+var app = builder.Build();
+{
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.yaml", "Open API");
+        });
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        // app.UseHsts();
+    }
+
+    // app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+
+    app.MapFallbackToFile("index.html");
+
+    // Database initialization process
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        DbInitializer.Seed(services);
     }
 }
+app.Run();
