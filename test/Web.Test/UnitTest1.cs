85172c866431f1d4396bdc9f2ee0ec93d9df4dc0
yasuaki-miyoshi <ikausay.443@gmail.com>
2022-03-24 22:32:36 +0900

    Format web test project

--- test/Web.Test/UnitTest1.cs
+++ test/Web.Test/UnitTest1.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using Xunit;
 
-namespace InspectionManager.Web.Test
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace InspectionManager.Web.Test;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
     }
 }
