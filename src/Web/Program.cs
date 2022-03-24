8c48c7e858ec451752e234c6faa5af90161e2cb1
yasuaki-miyoshi <ikausay.443@gmail.com>
2022-03-24 23:15:59 +0900

    Modify Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -63,12 +63,6 @@ var app = builder.Build();
             c.SwaggerEndpoint("/swagger/v1/swagger.yaml", "Open API");
         });
     }
-    else
-    {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        // app.UseHsts();
-    }
 
     // app.UseHttpsRedirection();
     app.UseStaticFiles();
